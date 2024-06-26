835afd23aab38a8eafd9bae7a8475cab4509de19
TriDuong070803 <132152293+TriDuong070803@users.noreply.github.com>
2024-06-26 11:43:59 +0700

    Update updateCompetitionStatus.js

--- src/services/updateCompetitionStatus.js
+++ src/services/updateCompetitionStatus.js
@@ -1,8 +1,8 @@
-const cron = require('node-cron');
-const CardService = require('./cardService'); // Adjust the path to your CardService
-const model= require('../models/Card');
+// const cron = require('node-cron');
+// const CardService = require('./cardService'); // Adjust the path to your CardService
+// const model= require('../models/Card');
 
-cron.schedule('0 0 * * *', () => {
- const cardService = new CardService(model);
- cardService.updateCompetitionStatus();
-});
+// cron.schedule('0 0 * * *', () => {
+//  const cardService = new CardService(model);
+//  cardService.updateCompetitionStatus();
+// });
